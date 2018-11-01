8e625e7b798c64e93f46b006a99965a67b898417
Robert <Robert@DESKTOP-NOK4FBL>
2018-11-01 23:31:49 +0000

    added timing fixed register issues when adding

--- 68K_Emulator/src/main.cpp
+++ 68K_Emulator/src/main.cpp
@@ -1,7 +1,9 @@
+#include <ctime>
 #include <console.h>
 #include <object.h>
-#include "io/io.h"
 
+
+#include "io/io.h"
 #include "mc68/mc68k.h"
 #include "lexer/lexer.h"
 #include "compiler/compiler.h"
@@ -21,9 +23,16 @@ int main(int argc, const char** argv)
 	//eventually load the compiled program into the emulator
 	mc68K->load(lexer->m_ProgramSourceLines);
 	
+	clock_t begin = clock();
+
 	mc68K->execute();
 
+	clock_t end = clock();
+	double elapsed_milli_secs = double(end - begin) / CLOCKS_PER_SEC * 1000;
+
 	mc68K->dumpRegisters();
+
+	CONSOLE.writeLine("exe time: %.5f", elapsed_milli_secs);
 
 	CONSOLE.readLine();
 }
